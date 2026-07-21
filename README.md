702818e2c74afdfc49d50c688514d4f208f2cd53
多羅狼 <Dorowolf314@gmail.com>
2026-07-21 18:25:51 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# chess
+# chess dragon
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/chess-dragon/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
